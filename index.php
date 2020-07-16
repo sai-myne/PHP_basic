5cfde15f146ab01a15acfdcf206e161f8ba6913d
sai-myne <ahlusir@gmail.com>
2020-07-16 12:36:14 +0800

    Boolean_and_comparisons

--- index.php
+++ index.php
@@ -1,37 +1,32 @@
 <?php 
 
-	// loops
+	
+	// comparisons booleans (true or false)
+	// echo true; "1"
+	// echo false; 
+	// numbers
+	// echo 5 < 10;
+	// echo 5 > 10;
+	// echo 5 == 10;
+	// echo 10 == 10;
+	// echo 5 != 10;
+	// echo 5 <= 5;
+	// echo 5 >= 5;
 
-	$ninjas = ['shaun', 'ryu', 'yoshi'];
+	// strings 
+	// echo 'shaun' < 'yoshi';
+	// echo 'shaun' > 'yoshi';
+	// echo 'shaun' > 'Shaun';
+	// echo 'mario' == 'mario';
+	// echo 'mario' == 'Mario';
+	
 
-	// for($i=0; $i<count($ninjas); $i++){
-	// 	echo $ninjas[$i]."<br />";
-	// }
-	// foreach($ninjas as $ninja){
-	// 	echo $ninja . "<br />";
-	// }
-
-	$products = [
-		['name' => 'shiny star', 'price' => 20],
-		['name' => 'green shell', 'price' => 10],
-		['name' => 'red shell', 'price' => 15],
-		['name' => 'gold coin', 'price' => 5],
-		['name' => 'lightning bolt', 'price' => 40],
-		['name' => 'banana skin', 'price' => 2]
-	];
-
-	// foreach($products as $product){
-	// 	echo $product['name'].' - '.$product['price'];
-	// 	echo '<br />';
-	// }
-	// $i=0;
-
-	// while($i < count($products)){
-	// 	echo $products[$i]['name'].' - '.$products[$i]['price'];
-	// 	echo '<br />';
-	// 	$i++;
-	// }
-
+	// loose vs strict equal comparison
+	// echo 5 == '5';
+	// echo 5 === '5';
+	// echo 5 === 5;
+	// echo true == '1';
+	echo false == '';
 
 ?>
 
@@ -42,13 +37,7 @@
 </head>
 <body>
 
-	<h1>Products</h1>
-	<ul>
-		<?php foreach($products as $product){ ?>
-			<h3><?php echo $product['name']; ?></h3>
-			<p>$ <?php echo $product['price']; ?></p>
-		<?php } ?>
-	</ul>
+	
 	
 
 </body>
