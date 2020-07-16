66929c474af9e339b7c4160d1cb5ade4a091bb45
sai-myne <ahlusir@gmail.com>
2020-07-16 13:39:00 +0800

    Functions

--- index.php
+++ index.php
@@ -1,39 +1,22 @@
 <?php 
 
 	
-	// conditional statemenst
+	// functions
 
-	// $price = 20;
-
-	// if ($price < 10){
-	// 	echo 'the condition is met';
-	// } elseif($price < 30){
-	// 	echo 'elseif condition met';
-	// }else {
-	// 	echo 'the conditon is not met';
-	// }
-
-	$products = [
-		['name' => 'shiny star', 'price' => 20],
-		['name' => 'green shell', 'price' => 10],
-		['name' => 'red shell', 'price' => 15],
-		['name' => 'gold coin', 'price' => 5],
-		['name' => 'lightning bolt', 'price' => 40],
-		['name' => 'banana skin', 'price' => 2]
-	];
-
-	foreach($products as $product){
-
-		if($product['name'] === 'lightning bolt'){
-			break;
-		}
-		if($product['price'] > 15){
-			continue;
-		}
-
-		echo $product['name'] . '<br />';
-
+	function sayHello($name='shaun', $time="morning"){
+		echo "good $time $name";
 	}
+
+	// sayHello('mario');
+	sayHello('yoshi', 'night');
+
+	function formatProduct($product){
+		// echo "{$product['name']} costs {$product['price']} to buy <br />";
+		return "{$product['name']} costs {$product['price']} to buy <br />";
+	}
+
+	// $formatted = formatProduct(['name' => 'gold star', 'price' => 20]);
+	// echo $formatted;
 
 ?>
 
