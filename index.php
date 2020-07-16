3d1cbd34f43e67c09826e1c7f386a3e5e2c91043
sai-myne <ahlusir@gmail.com>
2020-07-16 15:48:52 +0800

    Getting_data_from_a_database

--- index.php
+++ index.php
@@ -7,6 +7,23 @@
 	if(!$conn){
 		echo "Connection error: " . mysqli_connect_error();
 	}
+
+	// write query for all pizzas
+	$sql = 'SELECT title, ingredients, id FROM pizzas';
+
+	// make query & get result
+	$result = mysqli_query($conn, $sql);
+
+	// fetch the resulting rows as an array
+	$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+	// free result from memory
+	mysqli_free_result($result);
+
+	// close connection
+	mysqli_close($conn);
+
+	print_r($pizzas);
 	
 
 ?>
