48b9927f7ab98a6e18bddf395bc34b1c3fe58db2
sai-myne <ahlusir@gmail.com>
2020-07-16 15:41:06 +0800

    Connecting_to_a_Database

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php 
 
-	
+	// connect to database
+	$conn = mysqli_connect('localhost', 'shaun', 'test1234', 'ninja_pizza');
+
+	// check connection
+	if(!$conn){
+		echo "Connection error: " . mysqli_connect_error();
+	}
 	
 
 ?>
@@ -9,7 +15,7 @@
 <html>
 
 	<?php include 'templates/header.php'; ?>
-	
+
 	<?php include 'templates/footer.php'; ?>
 
 	
