75cec64b03b567db1066a805a5bd19e67c3886a2
sai-myne <ahlusir@gmail.com>
2020-07-16 14:30:16 +0800

    GET_and_POST

--- index.php
+++ index.php
@@ -8,8 +8,9 @@
 <!DOCTYPE html>
 <html>
 
-<?php include 'templates/header.php'; ?>
-<?php include 'templates/footer.php'; ?>
+	<?php include 'templates/header.php'; ?>
+	
+	<?php include 'templates/footer.php'; ?>
 
 	
 	
