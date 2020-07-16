ffb5ed76846ab598e1bf97f7e2f379290cb7c1ed
sai-myne <ahlusir@gmail.com>
2020-07-16 10:18:01 +0800

    first_php_file

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+	
+	// echo 'hello, ninjas';
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My first PHP file</title>
+</head>
+<body>
+
+	<h1><?php echo 'hello, ninjas'; ?></h1>
+
+</body>
+</html>
