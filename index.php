9c7ad68749f237b54f755add1d59991122c68b9a
sai-myne <ahlusir@gmail.com>
2020-07-16 15:59:01 +0800

    Redering_data_from_a_database

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	}
 
 	// write query for all pizzas
-	$sql = 'SELECT title, ingredients, id FROM pizzas';
+	$sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
 
 	// make query & get result
 	$result = mysqli_query($conn, $sql);
@@ -23,7 +23,6 @@
 	// close connection
 	mysqli_close($conn);
 
-	print_r($pizzas);
 	
 
 ?>
@@ -32,6 +31,26 @@
 <html>
 
 	<?php include 'templates/header.php'; ?>
+
+	<h4 class="center grey-text">Pizzas!</h4>
+
+	<div class="container">
+		<div class="row">
+			<?php foreach($pizzas as $pizza) { ?>
+				<div class="col s6 md3">
+					<div class="card z-depth-0">
+						<div class="card-content center">
+							<h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+							<div><?php echo htmlspecialchars($pizza['ingredients']) ?></div>
+							<div class="card-action right-align">
+								<a class="brand-text" href="#">more info</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			<?php }; ?>
+		</div>
+	</div>
 
 	<?php include 'templates/footer.php'; ?>
 
