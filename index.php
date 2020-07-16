196a80c4189d990a6be3655fc9d47898e1f3e2b8
sai-myne <ahlusir@gmail.com>
2020-07-16 12:04:08 +0800

    multidimensionArrays

--- index.php
+++ index.php
@@ -1,39 +1,20 @@
 <?php 
 
-	// indexed arrays
+	// multi-dimensional arrays
 	
-	$peopleOne = ['shaun', 'crystal', 'ryu'];
-	// echo $peopleOne[1]
+	$blogs = [
+		['title' => 'mario party', 'author'=>'mario', 'content'=>'lorem', 'likes'=>30],
+		['title' =>'mairo kart cheats', 'author'=>'toad', 'content'=>'lorem', 'likes'=>25],
+		['title' =>'zelda hidden chests', 'author'=>'link', 'content'=>'lorem', 'likes'=>50]
+	];
 
-	$peopleTwo = array('ken', 'chun-li');
-	// echo $peopleTwo[1];
-
-	$ages = [20,30,40,50];
-	// print_r($ages);
-
-	$ages[1] = 25;
-	// print_r($ages);
-
-	$ages[] = 60;
-	// print_r($ages);
-	array_push($ages, 70);
-	// print_r($ages);
-	// echo count($ages);
-	$peopleThree = array_merge($peopleOne, $peopleTwo);
-	// print_r($peopleThree);
-
-	// associative arrays (key & value pairs)
-	$ninjasOne = ['shaun'=>'black', 'mario' => 'orange', 'luigi' => 'brown'];
-	// echo $ninjasOne['mario'];
-	// print_r($ninjasOne);
-
-	$ninjasTwo = array('browser' => 'green', 'peach' => 'yellow');
-	// print_r($ninjasTwo);
-
-	$ninjasTwo['peach']='pink';
-	// print_r($ninjasTwo);
-	$ninjasThree = array_merge($ninjasOne, $ninjasTwo);
-	print_r($ninjasThree);
+	// print_r($blogs[1][1]);
+	// echo $blogs[2]['author'];
+	// echo count($blogs);
+	$blogs[] = ['title' => 'castle party', 'author' => 'peach', 'content'=> 'lorem', 'likes'=>100];
+	// print_r($blogs);
+	$popped = array_pop($blogs);
+	print_r($blogs);
 
 
 ?>
