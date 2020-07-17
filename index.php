ef1de32ca89d5963c56cf08d0cab7c54748957a6
sai-myne <ahlusir@gmail.com>
2020-07-17 09:33:07 +0800

    Control_Flow_Alt_Syntax

--- index.php
+++ index.php
@@ -38,15 +38,15 @@
 
 	<div class="container">
 		<div class="row">
-			<?php foreach($pizzas as $pizza) { ?>
+			<?php foreach($pizzas as $pizza): ?>
 				<div class="col s6 md3">
 					<div class="card z-depth-0">
 						<div class="card-content center">
 							<h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
 							<div><ul>
-								<?php foreach (explode(',', $pizza['ingredients']) as $ing) { ?>
+								<?php foreach (explode(',', $pizza['ingredients']) as $ing): ?>
 									<li><?php echo htmlspecialchars($ing); ?></li>
-								<?php } ?>
+								<?php endforeach; ?>
 							</ul></div>
 							<div class="card-action right-align">
 								<a class="brand-text" href="#">more info</a>
@@ -54,7 +54,13 @@
 						</div>
 					</div>
 				</div>
-			<?php }; ?>
+			<?php endforeach; ?>
+
+			<?php if(count($pizzas) >= 3): ?>
+				<p>there are 2 or more pizzas</p>
+			<?php  else:  ?>
+				<p>There are less than 3 pizzas</p>
+			<?php endif; ?>
 		</div>
 	</div>
 
