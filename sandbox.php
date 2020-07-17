5f55ada025aca160d260724feb54848f6238cdcd
sai-myne <ahlusir@gmail.com>
2020-07-17 10:53:24 +0800

    Cookies

--- sandbox.php
+++ sandbox.php
@@ -3,6 +3,9 @@
 	// sessions
 	if(isset($_POST['submit'])){
 
+		// cookie for gender
+		setcookie('gender', $_POST['gender'], time() + 86400);
+
 		session_start();
 
 		$_SESSION['name'] = $_POST['name'];
@@ -23,6 +26,10 @@
 
 	<form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
 		<input type="text" name="name">
+		<select name="gender">
+			<option value="male">male</option>
+			<option value="female">female</option>
+		</select>
 		<input type="submit" name="submit" value="submit">
 	</form>
 
