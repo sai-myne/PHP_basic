3680ba804cc06c87f544d4e9cd24c68b6fcad078
sai-myne <ahlusir@gmail.com>
2020-07-17 12:46:48 +0800

    classes_part2

--- sandbox.php
+++ sandbox.php
@@ -4,8 +4,8 @@
 
 	class User {
 
-		public $email;
-		public $name;
+		private $email;
+		private $name;
 
 		public function __construct($name, $email){
 			// $this->email = 'mario@thenetninja.co.uk';
@@ -19,18 +19,29 @@
 			echo $this->name . ' logged in';
 		}
 
+		public function getName(){
+			return $this->name;
+		}
+
+		public function setName($name){
+			if(is_string($name) && strlen($name) > 1){
+				$this->name = $name;
+				return "name has been updated to $name";
+			} else {
+				return 'not a valid name';
+			}
+		}
+
+
 
 	}
 
-	// $userOne = new User();
-
-	// $userOne->login();
-	// echo $userOne->email;
-
+	
 	$userTwo = new User('yoshi', 'yoshi@thenetninja.co.uk');
-	// echo $userTwo->name;
-	// echo $userTwo->email;
-	$userTwo->login();
+	// echo $userTwo->getName();
+	// echo $userTwo->setName(50);
+	$userTwo->setName('shaun');
+	echo $userTwo-> getName();
 	
 
 ?>
