20c648aa5465ba6d48473b3f438baad4d1b25524
sai-myne <ahlusir@gmail.com>
2020-07-17 10:28:17 +0800

    ternary_operator

---
+++ sandbox.php
@@ -0,0 +1,27 @@
+<?php 
+
+	// ternary operators
+	$score = 30;
+
+	// if($score > 40){
+	// 	echo 'high score!';
+	// } else {
+	// 	echo 'low score!';
+	// }
+
+	// echo $score > 40 ? 'high score!' : 'low score!';
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<p><?php echo $score > 40 ? 'high score!' : 'low score!'; ?></p>
+
+</body>
+</html>
